392495e8d3b675d51de807d7bc8e4bde593d5d45
NNJambetGu <guillaume.jambet@nespresso.com>
2015-08-17 00:08:57 +0200

    setting up dummy frontend

--- src/main/java/net/guillaume/feedback/WebConfig.java
+++ src/main/java/net/guillaume/feedback/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
@@ -24,8 +25,14 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     }
 
     @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/js/**").addResourceLocations("/js/");
+        registry.addResourceHandler("/**/*.html").addResourceLocations("/static/");
     }
 
 }
